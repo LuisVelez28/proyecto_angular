454edfe86528fde92fbfe1db9e8d63dfc50784d1
LuisVelez28 <luvelezg@unal.edu.co>
2026-02-25 11:34:59 -0500

    Primeros paso con Angular e integración de Bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pruebas } from "./pruebas/pruebas";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Pruebas],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/pruebas/pruebas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pruebas',
+  imports: [],
+  templateUrl: './pruebas.html',
+  styleUrl: './pruebas.scss',
+})
+export class Pruebas {
+
+}
